185ebdb35a8f235a94d12ecb6fb4a6db8b77048d
Pietro Mazzini <36486557+myasn1k@users.noreply.github.com>
2024-06-04 17:01:03 +0200

    Update tec-report link

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ export default function Home () {
       <img className='py-10 w-96' src='data-model.png' alt='Data model' />
       <Link href='/main' target='_blank' rel='noreferrer' className='p-2 m-1 border hover:bg-slate-100'>Visualization platform</Link>
       <Link href='https://github.com/smart-contracts-vulns-data-model/SmartBugs-Orchestrator' target='_blank' rel='noreferrer' className='p-2 m-1 border hover:bg-slate-100'>SmartBugs orchestrator</Link>
-      <Link href='https://github.com/smart-contracts-vulns-data-model/smart-vulnerabilities/blob/main/SoK__A_Unified_Data_Model_for_Smart_Contract_Vulnerability_Taxonomies.pdf' target='_blank' rel='noreferrer' className='p-2 m-1 border hover:bg-slate-100'>Technical report</Link>
+      <Link href='https://github.com/ds-square/smart-vulnerabilities/blob/main/SoK__A_Unified_Data_Model_for_Smart_Contract_Vulnerability_Taxonomies.pdf' target='_blank' rel='noreferrer' className='p-2 m-1 border hover:bg-slate-100'>Technical report</Link>
     </div>
   )
 }

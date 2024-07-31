b1f9079a45140106f985df95ae81e93eb6e861d8
Pietro Mazzini <36486557+myasn1k@users.noreply.github.com>
2024-07-31 17:40:46 +0200

    Update page.js - Add DOI

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ export default function Home () {
   return (
     <div className='flex flex-col items-center justify-center w-full text-lg'>
       <h1 className='pb-4 text-2xl font-bold text-center text-emerald-800'>SoK: A Unified Data Model for<br />Smart Contract Vulnerability Taxonomies</h1>
+      <h2 className='text-lg font-bold text-slate-400'><a href="https://doi.org/10.1145/3664476.3664507">DOI: 10.1145/3664476.3664507</a></h2>
       <h2 className='text-lg font-bold text-slate-400'>Claudia Ruggiero, Pietro Mazzini, Emilio Coppa, Simone Lenti, Silvia Bonomi</h2>
       <h2 className='text-lg font-bold text-slate-400'>ARES 2024</h2>
       <img className='py-10 w-96' src='data-model.png' alt='Data model' />
